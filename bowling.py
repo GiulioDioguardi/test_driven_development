8b45508a222a3025f259b25e77f6dd3dcff292c0
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2016-11-13 22:31:04 +0100

    Added test for spare

--- bowling.py
+++ bowling.py
@@ -12,6 +12,12 @@ class Game(object):
 
     def score(self):
         score = 0
-        for roll in self.rolls:
-            score += roll
+        frame_index = 0
+        for frame in range(10):
+            if self.rolls[frame_index] + self.rolls[frame_index + 1] == 10:
+                score += 10 + self.rolls[frame_index + 2]
+                frame_index += 2
+            else:
+                score += self.rolls[frame_index] + self.rolls[frame_index + 1]
+                frame_index += 2
         return score
--- bowling_test.py
+++ bowling_test.py
@@ -17,5 +17,16 @@ class GameTest(unittest.TestCase):
         self.roll_many(20, 0)
         self.assertEqual(0, self.g.score())
 
+    def test_all_ones(self):
+        self.roll_many(20, 1)
+        self.assertEqual(20, self.g.score())
+
+    def test_spare(self):
+        self.g.roll(5)
+        self.g.roll(5) #spare
+        self.g.roll(3)
+        self.roll_many(17, 0)
+        self.assertEqual(16, self.g.score())
+
 if __name__ == "__main__":
     unittest.main()
