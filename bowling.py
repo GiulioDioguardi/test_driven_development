cd0968183d4c2f20e3c5334c14a1c4c95ae997c8
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2016-11-13 22:47:36 +0100

    Added strike tests and perfect game

--- bowling.py
+++ bowling.py
@@ -13,12 +13,24 @@ class Game(object):
     def is_spare(self, findex):
         return self.rolls[findex] + self.rolls[findex + 1] == 10
 
+    def is_strike(self, findex):
+        return self.rolls[findex] == 10
+
+    def strike_bonus(self, findex):
+        return self.rolls[findex + 1] + self.rolls[findex + 2]
+
+    def spare_bonus(self, findex):
+        return self.rolls[findex + 2]
+
     def score(self):
         score = 0
         frame_index = 0
         for frame in range(10):
-            if self.is_spare(frame_index):
-                score += 10 + self.rolls[frame_index + 2]
+            if self.is_strike(frame_index):
+                score += 10 + self.strike_bonus(frame_index) 
+                frame_index += 1
+            elif self.is_spare(frame_index):
+                score += 10 + self.spare_bonus(frame_index)
                 frame_index += 2
             else:
                 score += self.rolls[frame_index] + self.rolls[frame_index + 1]
--- bowling_test.py
+++ bowling_test.py
@@ -30,5 +30,22 @@ class GameTest(unittest.TestCase):
         self.roll_many(17, 0)
         self.assertEqual(16, self.g.score())
 
+    def test_no_spare(self):
+        self.g.roll(2)
+        self.g.roll(6)
+        self.g.roll(4)
+        self.roll_many(17, 0)
+        self.assertEqual(12, self.g.score())
+
+    def test_strike(self):
+        self.g.roll(10)
+        self.g.roll(6)
+        self.g.roll(3)
+        self.assertEqual(28, self.g.score())
+
+    def test_perfect_game(self):
+        self.roll_many(12, 10)
+        self.assertEqual(300, self.g.score())
+
 if __name__ == "__main__":
     unittest.main()
