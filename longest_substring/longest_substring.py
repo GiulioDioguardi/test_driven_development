eab8eed88341bcc3dcc3f41232944bb30fb56770
Giulio Dioguardi <giulio.dioguardi@alten.nl>
2017-12-20 22:12:42 +0100

    Added unit tests for longest substring

---
+++ longest_substring/longest_substring.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+def length_of_longest_substring(s):
+    longest_substring = 0
+    current_substring = ""
+    for c in s:
+        if c in current_substring:
+            current_length = len(current_substring)
+            longest_substring = max(current_length, longest_substring)
+            current_substring = ""
+        else:
+            current_substring += c
+    return max(current_length, longest_substring)
--- longest_substring/unit_test.py
+++ longest_substring/unit_test.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
 import unittest
+from longest_substring import length_of_longest_substring
 
 class TestLongestSubstring(unittest.TestCase):
-    def test_nothing(self):
-        pass
+    def test_longest_substring_1(self):
+        self.assertEquals(1, length_of_longest_substring("bbbbb"))
+
+    def test_can_get_longest_substring_3(self):
+        string = "abcabcbb" #longest is 'abc'
+        self.assertEquals(3, length_of_longest_substring(string))
 
 if __name__ == "__main__":
     unittest.main()
