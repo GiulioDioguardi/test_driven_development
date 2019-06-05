9590d6bd5ac4a37f4ded36db160f21e760689202
giulio-dioguardi <giulio.dioguardi@alten.nl>
2019-06-05 17:50:41 +0200

    All works

--- primes/primes.py
+++ primes/primes.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python
 
 def factortoprimes(number):
-    return []
+    if number is None:
+        return []
+
+    primes = []
+    devisor = 2
+    while number > 1:
+        if number % devisor == 0:
+            primes.append(devisor)
+            number /= devisor
+        else:
+            devisor += 1
+    return primes
--- primes/unit_test.py
+++ primes/unit_test.py
@@ -10,6 +10,17 @@ class TestPrimes(unittest.TestCase):
 
     def test_primes(self):
         self.assertcorrectprimes(None, [])
+        self.assertcorrectprimes(1, [])
+        self.assertcorrectprimes(2, [2])
+        self.assertcorrectprimes(3, [3])
+        self.assertcorrectprimes(4, [2,2])
+        self.assertcorrectprimes(5, [5])
+        self.assertcorrectprimes(6, [2,3])
+        self.assertcorrectprimes(7, [7])
+        self.assertcorrectprimes(8, [2,2,2])
+        self.assertcorrectprimes(9, [3,3])
+        self.assertcorrectprimes(3*13*23*31, [3,13,23,31])
+
 
 if __name__ == "__main__":
     unittest.main()
