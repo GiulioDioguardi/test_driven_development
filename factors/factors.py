6279ab8d340c50ba4fa4d899ba80b2c7d265663d
Giulio Dioguardi <giulio.dioguardi@delem.com>
2021-07-21 10:30:32 +0200

    GIU-000 Transfromed factors to python version 3.6

--- factors/factors.py
+++ factors/factors.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import optparse
+import argparse
 import math
 
 def factors_of(number):
@@ -14,14 +14,12 @@ def factors_of(number):
     return factors
 
 def get_parser():
-    parser = optparse.OptionParser()
-    parser.add_option("-n", "--number", type="int",
-        help="the number to factor out")
-    parser.add_option("--search-primes", type='int',
-        help="Get a list of primes, maximum of a given number")
-    parsed_args = parser.parse_args()[0]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--number", type=int, help="the number to factor out")
+    parser.add_argument("--search-primes", type=int, help="Get a list of primes, maximum of a given number")
+    parsed_args = parser.parse_args()
     if parsed_args.number is None and parsed_args.search_primes is None:
-        print "No number given to factor out, or no searched primes"
+        print("No number given to factor out, or no searched primes")
         parser.print_help()
         exit(1)
     return parsed_args
@@ -30,13 +28,13 @@ def search_primes(max):
     number = 1
     while max > 0:
         if len(factors_of(number)) == 1:
-            print number
+            print(number)
         number += 1
         max -= 1
 
 if __name__ == "__main__":
-    parser = get_parser()
-    if parser.number:
-        print factors_of(parser.number)
-    if parser.search_primes:
+    args = get_parser()
+    if args.number:
+        print(factors_of(parser.number))
+    if args.search_primes:
         search_primes(parser.search_primes)
